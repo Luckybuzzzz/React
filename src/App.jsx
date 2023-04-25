4853e140a52f810fd9d04af07f7cffd11882a785
luckybuzzzz <luckybuzzz@icloud.com>
2023-04-25 15:25:31 +0300

    new version

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Slider from './components/Slider';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+    <Header></Header>
+    <Slider></Slider>
+    </div>
+  );
+}
+
+export default App;
